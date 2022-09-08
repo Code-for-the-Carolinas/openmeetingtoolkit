2c7171c5ec4ac003efd52ab48f600eee04a83f48
dernewt <dernewt@gmail.com>
2022-09-08 19:49:20 -0400

    try harder to filter out old meetings

--- publicmeetings/Scrapers/ReusableXpathScraper/Scrapers/TempralExpression.cs
+++ publicmeetings/Scrapers/ReusableXpathScraper/Scrapers/TempralExpression.cs
@@ -1,4 +1,6 @@
-﻿namespace Scrapers;
+﻿using System.Globalization;
+
+namespace Scrapers;
 
 public class TempralExpression
 {
@@ -16,6 +18,12 @@ public class TempralExpression
             if (rawAsDate < after)
                 return null;
             return rawAsDate;
+        }
+        else if(DateTime.TryParseExact(Raw, "M/d/yyyy h:mm:ss tt \"America/New_York\"", CultureInfo.CurrentCulture, DateTimeStyles.AllowWhiteSpaces, out var longDate))
+        {//        12/24/2012 12:00:00 AM America/New_York
+            if (longDate < after)
+                return null;
+            return longDate;
         }
 
         return after; ///TODO need to actually parse expression
