a31e089774364437a618a680d4473fafa5e40ee9
dernewt <dernewt@gmail.com>
2022-09-21 21:50:35 -0400

    fix error with importing blank rows from manual spreadsheet. Add system for manual corrections.

---
+++ publicmeetings/Scrapers/ReusableXpathScraper/ReusableXpathScraper/MappableMeetingHelper.cs
@@ -0,0 +1,59 @@
+﻿using Scrapers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public static class MappableMeetingHelper
+{
+    public static async Task AddManualMeetings(this List<MappableMeeting> meetings, string manualMeetingFile, IHost host)
+    {
+        var meetingFactory = host.Services.GetRequiredService<CachingMeetingFactory>();
+        await foreach (var meeting in manualMeetingFile.FromCsv(meetingFactory))
+        {
+            meetings.Add(meeting);
+        }
+    }
+
+    public static IEnumerable<MappableMeeting> MergeCorrections(this List<MappableMeeting> meetings, List<MeetingCorrection> corrections)
+    {
+        foreach (var meeting in meetings)
+        {
+            var foundMatch = false;
+            foreach (var correction in corrections)
+            {
+                if (correction.Matches(meeting))
+                {
+                    foundMatch = true;
+                    yield return meeting.Merge(correction);
+                }
+            }
+            if(!foundMatch)
+                yield return meeting;
+        }
+    }
+
+    public static async Task AddScrapedMeetings(this List<MappableMeeting> meetings, IEnumerable<ScrapeTarget> targets, IHost host)
+    {
+        var calendar = host.Services.GetRequiredService<CalendarService>();
+        var scraper = host.Services.GetRequiredService<Scraper>();
+        var meetingFactory = host.Services.GetRequiredService<CachingMeetingFactory>();
+
+        foreach (var target in targets)
+        {
+            try
+            {
+                var results = scraper.Scrape(target);
+                await foreach (var result in results)
+                {
+                    var mappedResult = await meetingFactory.GetMappableMeeting(result);
+
+                    if (mappedResult.IsCurrent(calendar.Now))
+                        meetings.Add(mappedResult);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"IGNORED {target.County} because {e.Message}");
+            }
+        }
+    }
+}
--- publicmeetings/Scrapers/ReusableXpathScraper/ReusableXpathScraper/Program.cs
+++ publicmeetings/Scrapers/ReusableXpathScraper/ReusableXpathScraper/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Scrapers;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using ReusableXpathScraper;
 using Microsoft.Extensions.Options;
 
@@ -18,44 +17,10 @@ var mapMeetings = new List<MappableMeeting>(1000);
 await mapMeetings.AddManualMeetings(settings.ManualMeetingFile, host);
 await mapMeetings.AddScrapedMeetings(NorthCarolinaScrapeTarget.All, host);
 
+var corrections = settings.ManualCorrectionsFile.FromCsv();
+mapMeetings = mapMeetings.MergeCorrections(corrections)
+    .ToList();
+
 await File.WriteAllTextAsync(settings.MeetingSaveFile, mapMeetings.ToJson());
 
 await File.WriteAllTextAsync(settings.MeetingSaveBaserowFile, mapMeetings.ToCsv());
-
-public static class MappableMeetingHelper
-{
-    public static async Task AddManualMeetings(this List<MappableMeeting> meetings, string manualMeetingFile, IHost host)
-    {
-        var meetingFactory = host.Services.GetRequiredService<CachingMeetingFactory>();
-        await foreach (var meeting in manualMeetingFile.FromCsv(meetingFactory))
-        {
-            meetings.Add(meeting);
-        }
-    }
-
-    public static async Task AddScrapedMeetings(this List<MappableMeeting> meetings, IEnumerable<ScrapeTarget> targets, IHost host)
-    {
-        var calendar = host.Services.GetRequiredService<CalendarService>();
-        var scraper = host.Services.GetRequiredService<Scraper>();
-        var meetingFactory = host.Services.GetRequiredService<CachingMeetingFactory>();
-
-        foreach (var target in targets)
-        {
-            try
-            {
-                var results = scraper.Scrape(target);
-                await foreach (var result in results)
-                {
-                    var mappedResult = await meetingFactory.GetMappableMeeting(result);
-
-                    if (mappedResult.IsCurrent(calendar.Now))
-                        meetings.Add(mappedResult);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"IGNORED {target.County} because {e.Message}");
-            }
-        }
-    }
-}
